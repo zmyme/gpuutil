432dec365e20f675d026867a8f8c3af02e63422b
zmy <izmy@qq.com>
2020-11-17 21:09:11 +0800

    update to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'gpuutil',
-    version = '0.0.1',
+    version = '0.0.2',
     keywords='gpu utils',
     description = 'A tool for observing gpu stat and auto set visible gpu in python code.',
     license = 'MIT License',
